7af78545c1c02ea51765fcfcaea26970fbdcadb1
tong <bluedragon00000@gmail.com>
2025-03-30 03:53:34 +0800

    Update tsconfig.json to add path alias for navigation components and refactor import statement in Layout component for improved clarity and maintainability. This change adheres to best practices and enhances code organization.

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Footer } from "@/components/layout/footer/Footer";
-import { NavBar } from "@/components/navigation/NavBar";
+import { NavBar } from "../navigation/NavBar";
 
 interface LayoutProps {
   children: React.ReactNode;
